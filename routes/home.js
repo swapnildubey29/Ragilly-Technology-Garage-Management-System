460d61972b656ec7bbbd9f4571aa0a179a7b18a3
swapnildubey29 <swapnildubey3636@gmail.com>
2024-06-08 17:25:14 +0530

    update code

--- routes/home.js
+++ routes/home.js
@@ -1,26 +1,31 @@
-const express = require('express')
-const router = express.Router()
-const Order = require('../models/Order')
-const { User } = require('../models/User')
+const express = require("express");
+const router = express.Router();
+const Order = require("../models/Order");
+const { User } = require("../models/User");
 
 // Route to create a new order
-router.post('/job', async (req, res) => {
-    try {
-        const orderData = req.body;
-        console.log(orderData)
-        const { mobile } = orderData;
-        const newOrder = new Order(orderData)
-        await newOrder.save()
-        const existingUser = await User.findOne({ mobile })
-        if (existingUser) {
-            res.status(201).json({ success: true, redirect: '/login' })
-        } else {
-            res.status(201).json({ success: true, redirect: '/signup' })
-        }
-    } catch (error) {
-        console.error("Error creating order:", error)
-        res.status(500).json({ error: 'Internal server error' })
+router.post("/job", async (req, res) => {
+  try {
+    const orderData = req.body;
+    console.log(orderData);
+    const { mobile } = orderData;
+    const newOrder = new Order(orderData);
+    await newOrder.save();
+    const existingUser = await User.findOne({ mobile });
+    if (existingUser) {
+      res.status(201).json({ success: true, redirect: "/login" });
+    } else {
+      res.status(201).json({ success: true, redirect: "/signup" });
     }
-})
+  } catch (error) {
+    console.error("Error creating order:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+// Logout
+router.post("/logout", (req, res) => {
+  res.clearCookie("token");
+  res.status(200).json({ success: true, message: "Logout successful" });
+});
 
 module.exports = router;
